8bd2a2e3bd1b595157ada30d1fd954ff57f659fd
zipppzy <zipppzy939@gmail.com>
2017-05-07 16:00:36 -0500

    finished genAlg

--- MK2.py
+++ MK2.py
@@ -117,13 +117,17 @@ class GenAlg():
                 self.culledPercent=culledPercent
                 self.structure=structure
                 self.numInputs=numInputs
-                #self.sigmoidConstant=sigmoidConstant
-                #self.stepLimit=stepLimit
+                self.sigmoidConstant=sigmoidConstant
+                self.stepLimit=stepLimit
                 self.gen=[]
                 for i in range(numIndivs):
                         self.gen.append(NeuralNet(structure,numInputs,activationFunc,sigmoidConstant,stepLimit))
                 for i in self.gen:
                         i.setWeights(self.randomWeights())
+                for i in range(numGens):
+                	self.cull()
+                	self.reproduce()
+
                 
 
         def randomWeights(self):
@@ -150,23 +154,58 @@ class GenAlg():
         def cull(self):
                 self.gen.sort(key=operator.attrgetter("fitness"),reverse=True)
                 self.gen=self.gen[1:int(len(self.gen)*self.culledPercent)]
-        #def reproduce(self):
+        def reproduce(self):
+        	culledNum=len(self.gen)
+        	while(len(self.gen)<self.numIndivs):
+        		choice1=int(random.random()*culledNum)
+        		choice2=int(random.random()*culledNum)
+        		while(choice2==choice1):
+        			choice2=int(random.random()*culledNum)
+        		children=self.crossover(self.gen[choice1].getWeights(),gen[choice2].getWeights())
+        		self.gen.append(NeuralNet(self.structure,self.numInputs,self.activationFunc,self.sigmoidConstant,self.stepLimit))
+        		self.gen[len(self.gen)-1].setWeights(children[0])
+        		self.gen.append(NeuralNet(self.structure,self.numInputs,self.activationFunc,self.sigmoidConstant,self.stepLimit))
+        		self.gen[len(self.gen)-1].setWeights(children[1])
+        	if(len(self.gen)>self.numIndivs):
+        		del self.gen[len(self.gen)-1]
                 
-        def crossover(self):
+        def crossover(self,A,B):
+        	weightsA=self.threeDtoOneD(A)
+        	weightsB=threeDtoOneD(B)
+        	cutPoint=int(random.random()*(len(weightsA)-1))+1
+        	piece1A=weightsA[0:cutPoint]
+        	piece2A=weightsA[cutPoint:len(weightsA)]
+        	piece1B=weightsB[0:cutPoint]
+        	piece2B=weightsB[cutPoint:len(weightsB)]
+        	child1=piece1A+piece2B
+        	child2=peice1B+piece2A
+        	for i in child1:
+        		if(random.random()<self.mutationRate):
+        			i=random.random()
+        	child2=peice1B+piece2A
+        	for i in child2:
+        		if(random.random()<self.mutationRate):
+        			i=random.random()
+        	child1=oneDtoThreeD(child1)
+        	child2=oneDtoThreeD(child2)
+        	return [child1,child2]
+
+
+
         
 
-        def 3Dto1D(3D):
-                1D=[]
-                for i in 3D:
+        def threeDtoOneD(self,three):
+                one=[]
+                for i in three:
                         for j in i:
                                 for k in j:
-                                        1D.append(k)
+                                        one.append(k)
                 return b
-        def 1Dto3D(self,1D):
-                1DCounter=0
-                3D=[]
+        def oneDtoThreeD(self,one):
+                oneCounter=0
+                three=[]
                 for i in range(len(self.structure)):
-                        3D.append([])
+                        three.append([])
                 for i in range(len(self.structure)):
                         
                         for j in range(self.structure[i]):
@@ -174,18 +213,18 @@ class GenAlg():
                                 neuronWeights=[]
                                 if (i==0):
                                         for i in range(self.numInputs):
-                                                neuronWeights.append(1D[1DCounter])
-                                                1DCounter++
+                                                neuronWeights.append(one[oneCounter])
+                                                oneCounter+=1
                                                 
                                 elif(i>0):
                                         for i in range (self.structure[i-1]):
-                                                neuronWeights.append(1D[1DCounter])
-                                                1DCounter++
-                                3D[i].append(neuronWeights)                
-                                
-                                
-g=GenAlg(1,10,.1,.1,.1,[2,3],1,"sigmoid")
-g.cull()
+                                                neuronWeights.append(one[oneCounter])
+                                                oneCounter+=1
+                                three[i].append(neuronWeights)
+                return three                               
+                        
+g=GenAlg(1,10,.1,.2,.1,[2,3],1,"sigmoid")
+
 
 for i in g.gen:
         print(i.fitness)
